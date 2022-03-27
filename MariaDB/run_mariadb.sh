af31c339129e97e521cbfe03196e75d4be0d09aa
Médéris Frambourt <mframbou@student.42nice.fr>
2022-03-27 21:17:20 +0200

    WIP

--- MariaDB/run_mariadb.sh
+++ MariaDB/run_mariadb.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# Install db only one (if non-existent)
 # You should either run mysql_install_db with the same account that will be running mysqld or run it as root with --user
-mysql_install_db --user=root --datadir=/var/lib/mysql
+if [ ! -d /var/lib/mysql/mysql ]; then
+	ls -l /var/lib/mysql/mysql
+	echo "Installing database"
+	mysql_install_db --user=root --datadir=/var/lib/mysql
+fi
 
 mysqld --user=root
