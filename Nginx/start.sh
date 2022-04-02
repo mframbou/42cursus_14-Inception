bb1445bdbfafa1464453e3f27d461e192fa48d33
Médéris Frambourt <mframbou@student.42nice.fr>
2022-04-02 18:20:10 +0200

    https working over port 443

--- Nginx/start.sh
+++ Nginx/start.sh
@@ -4,7 +4,10 @@ if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
 	echo "Generating self-signed certificate"
 	# https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-16-04
 	# -subj to use arguments instead of interactive prompt, see https://www.shellhacks.com/create-csr-openssl-without-prompt-non-interactive/
+	# X509 = norm for public key certificates 
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=Le Royaume Des Cracks/L=Nice/O=Crackito Corp./OU=Departement des bg/CN=gigachad.fr"
+
+	echo "Successfully generated certificate"
 fi
 
 nginx -g 'daemon off;'
