a567db944202ecfd7973eebd5731b6270e8e247b
Médéris Frambourt <mframbou@student.42nice.fr>
2022-04-02 21:32:30 +0200

    Added subjectAltName

--- Nginx/start.sh
+++ Nginx/start.sh
@@ -5,7 +5,7 @@ if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
 	# https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-16-04
 	# -subj to use arguments instead of interactive prompt, see https://www.shellhacks.com/create-csr-openssl-without-prompt-non-interactive/
 	# X509 = norm for public key certificates 
-	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=Le Royaume Des Cracks/L=Nice/O=Crackito Corp./OU=Departement des bg/CN=gigachad.fr"
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=Le Royaume Des Cracks/L=Nice/O=Crackito Corp./OU=Departement des bg/CN=gigachad.fr" -addext "subjectAltName = DNS:127.0.0.1"
 
 	echo "Successfully generated certificate"
 fi
